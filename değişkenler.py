7141e47aa12a198257fc5371f25e5242364adcb8
Özgür Güngör <oozgurgungorr@gmail.com>
2022-12-31 17:15:56 +0300

    Add files via upload

---
+++ değişkenler.py
@@ -0,0 +1,56 @@
+
+
+
+print(5000 - (5000 * 0.27))
+
+
+print(4000 - (4000 * 0.27))
+
+
+
+#dfdsgdfjghjghjghj
+
+
+
+a = '10'
+b = '20'
+
+print(a+b) # 30=> 1020
+
+
+
+
+
+firstName = "Sadık"
+lastName = " Turan"
+
+print(firstName + lastName) #Sadık Turan
+
+
+
+
+
+
+"""
+2-Aşağıdaki siparişlerin toplam bilgisini hesaplayınız.
+
+Sipariş 1 => 110     toplam
+Sipariş 2 => 1100.5  toplam
+Sipariş 3 => 356.95  toplam
+
+"""
+
+
+order1 = 110
+order2 = 1100.5
+order3 = 356.95
+
+total = order1 + order2 + order3
+
+print("total:", total)
+
+
+
+
+
+
